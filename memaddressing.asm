64843554870d6878ebc7c72e3c0bd358f9aef53c
Kyle McCahill <kylemccahill@gmail.com>
2023-11-04 01:04:14 -0500

    made it work on windows with print

--- memaddressing.asm
+++ memaddressing.asm
@@ -1,14 +1,18 @@
-global main
+global _main
+extern _printf
 section .data
     addr db "yellow"
 
 section .text
-main:               ;; noticed some syntax stuff. the windows version needs _ prefix, linux no.
+_main:               ;; noticed some syntax stuff. the windows version needs _ prefix, linux no.
+    mov [addr], byte'H'
+    mov [addr+5], byte'!'
     mov eax, 4      ; sys_write system call
     mov ebx, 1      ; stdout file desriptor
-    mov ecx, addr   ; bytes to write.
-    mov edx, 6      ; number of bytes to write
-    int 0x80        ; syscall
+    mov ecx, addr   ; bytes to write
+    push ecx
+    call _printf
+
     mov eax, 1      ; sys_exit call
     mov ebx, 0      ; return 0 from program
     int 0x80
